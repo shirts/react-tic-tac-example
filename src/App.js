5a3c4e7d09ce2e45405eed1cfc2e69bcb1d9cd8f
Kevin Chandler <im.kevin@me.com>
2018-12-17 14:36:51 -0800

    init game

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Board from './Board'
+
+class App extends Component {
+  render() {
+    return (
+      <Board/>
+    );
+  }
+}
+
+export default App;
